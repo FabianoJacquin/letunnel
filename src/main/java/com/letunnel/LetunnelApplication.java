68a95f63dd19252f89c7f37939d26b9c1790d47a
Fabiano Jacquin <fabiano.jacquin@gmail.com>
2020-04-09 21:42:31 +0200

    Creato progetto Spring (Spring Initializr). Chiude #1

---
+++ src/main/java/com/letunnel/LetunnelApplication.java
@@ -0,0 +1,13 @@
+package com.letunnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetunnelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetunnelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/letunnel/LetunnelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letunnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetunnelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
