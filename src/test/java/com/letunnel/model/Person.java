626d162b3a852dc77158106d2932df82b7c1b148
Fabiano Jacquin <fabiano.jacquin@gmail.com>
2020-04-11 13:06:22 +0200

    Implementare UserSDJpaService. Close #5

--- src/test/java/com/letunnel/model/Person.java
+++ src/test/java/com/letunnel/model/Person.java
@@ -1,16 +1,45 @@
 package com.letunnel.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import lombok.Data;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 
-@Data
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @Column(name = "mail")
     private String mail;
 
+    public Person(Long id, String firstName, String lastName, String address, String city, String telephone, String mail) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+        this.mail = mail;
+    }
 }
--- src/test/java/com/letunnel/model/User.java
+++ src/test/java/com/letunnel/model/User.java
@@ -1,11 +1,31 @@
 package com.letunnel.model;
 
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User extends Person {
 
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "password")
     private String password;
 
+    @Builder
+    public User(String firstName, String lastName, String address, String city, String telephone, String mail, String username, String password) {
+        super(firstName, lastName, address, city, telephone, mail);
+        this.username = username;
+        this.password = password;
+    }
 }
