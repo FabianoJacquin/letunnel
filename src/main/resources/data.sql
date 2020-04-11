cf1b7bbb81236679af3358cc48ad71693ffc7d75
Fabiano Jacquin <fabiano.jacquin@gmail.com>
2020-04-11 14:03:40 +0200

    Load Bootstrap data on startup. Close #14

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (first_name, last_name, address, city, telephone,
+    mail, username, password)
+    VALUES ('Fabiano', 'Jacquin', 'Frazione Cerisey 21 - 11010 - Saint Rhemy en Bosses', 'Aosta', '347 3423385',
+    'fabiano.jacquin@gmail.com', 'fjacquin', '12021973');
