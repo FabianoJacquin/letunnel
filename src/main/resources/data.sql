c0584d2c9bef82dfcae2fdafe6948af496e3dd88
Fabiano Jacquin <fabiano.jacquin@gmail.com>
2020-04-11 14:07:02 +0200

    Caricare dati nel DB allo startup. Close #8

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,8 @@ INSERT INTO users (first_name, last_name, address, city, telephone,
     mail, username, password)
     VALUES ('Fabiano', 'Jacquin', 'Frazione Cerisey 21 - 11010 - Saint Rhemy en Bosses', 'Aosta', '347 3423385',
     'fabiano.jacquin@gmail.com', 'fjacquin', '12021973');
+
+INSERT INTO users (first_name, last_name, address, city, telephone,
+    mail, username, password)
+    VALUES ('Carole', 'Proment', 'Frazione Cerisey 21 - 11010 - Saint Rhemy en Bosses', 'Aosta', '328 3826219',
+    'carole.proment@gmail.com', 'cproment', '27031980');
